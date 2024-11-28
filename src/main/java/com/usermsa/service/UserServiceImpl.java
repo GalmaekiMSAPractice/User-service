31bff625acb705d54dcdc2eb1cc23d1ef83ae1f1
Galmaeki <kjh09800@gmail.com>
2024-11-28 23:51:32 +0900

    Spring Security 설정 추가

--- src/main/java/com/usermsa/service/UserServiceImpl.java
+++ src/main/java/com/usermsa/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.usermsa.dto.UserDto;
 import com.usermsa.entity.UserEntity;
 import com.usermsa.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -13,10 +14,11 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
+    private final PasswordEncoder passwordEncoder;
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
+        userDto.setEncryptPw(passwordEncoder.encode(userDto.getPw()));
         userRepository.save(new UserEntity(userDto));
         return userDto;
     }
